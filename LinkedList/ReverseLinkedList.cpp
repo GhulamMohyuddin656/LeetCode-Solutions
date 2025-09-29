773436d99e27b18f3875c8b828cb046a8b142f32
GhulamMohyuddin656 <gmudgatti99@gmail.com>
2025-09-29 17:40:52 +0500

    ReverseLinkedList.cpp

--- LinkedList/ReverseLinkedList.cpp
+++ LinkedList/ReverseLinkedList.cpp
@@ -1,1 +1,21 @@
+// Problem: LeetCode 206 - Reverse Linked List
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Approach: Iterative using three pointers (Prev, Curr, Next)
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* Prev = NULL;
+        ListNode* Curr = head;
+        ListNode* Next = NULL;
+        while (Curr != NULL) {
+            Next = Curr->next;
+            Curr->next = Prev;
+            Prev = Curr;
+            Curr = Next;
+        }
+        return Prev;
+    }
+};
 
